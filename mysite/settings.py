e0161ca02869ded3e339965b14eb4e45f12e888f
arad1367 <pejman.ebrahimi77@gmail.com>
2023-02-15 14:56:08 +0100

    delete image field

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "/workspace/static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
